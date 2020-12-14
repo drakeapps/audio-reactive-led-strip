18452939bb1278c2073b80f2d3e2fc3c7d3884bb
James Wilson <james@drakeapps.com>
2020-12-13 18:09:24 -0600

    fix mic class

--- AudioReactiveLEDStrip/microphone.py
+++ AudioReactiveLEDStrip/microphone.py
@@ -2,15 +2,13 @@ import time
 import numpy as np
 import pyaudio
 
-CONTINUE_MIC_STREAM = True
-
 class Microphone:
     def __init__(self):
         self.stream = None
+        self.p = None
         self.continue_mic_stream = True
 
     def start_stream(self, callback, mic_rate=44100, fps=60):
-        global CONTINUE_MIC_STREAM
         self.p = pyaudio.PyAudio()
         frames_per_buffer = int(mic_rate / fps)
         self.stream = self.p.open(format=pyaudio.paInt16,
@@ -22,9 +20,9 @@ class Microphone:
         prev_ovf_time = time.time()
         while self.continue_mic_stream:
             try:
-                y = np.fromstring(stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
+                y = np.fromstring(self.stream.read(frames_per_buffer, exception_on_overflow=False), dtype=np.int16)
                 y = y.astype(np.float32)
-                self.stream.read(stream.get_read_available(), exception_on_overflow=False)
+                self.stream.read(self.stream.get_read_available(), exception_on_overflow=False)
                 callback(y)
             except IOError:
                 overflows += 1
@@ -33,9 +31,12 @@ class Microphone:
                     print('Audio buffer has overflowed {} times'.format(overflows))
             except:
                 pass
-        self.stream.stop_stream()
-        self.stream.close()
-        self.p.terminate()
+        try:
+            self.stream.stop_stream()
+            self.stream.close()
+            self.p.terminate()
+        except:
+            pass
 
     def stop_stream(self):
         self.continue_mic_stream = False
